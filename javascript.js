5bd6d3da1afdea5e6b78c1b1ad13a476444db334
DaniloR05 <135133610+DaniloR05@users.noreply.github.com>
2023-06-01 22:29:55 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+
+	function validate(){
+	var username = document.getElementById("uname");
+	var password =	 document.getElementById("pass");
+	 if (username.value !=="danilo" || password.value!== "danilo"){
+	alert("Unesite podatke");
+	 return false;
+	 }
+	 else{
+		 
+true
+	}
+	}
+	 
+	
